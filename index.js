722c3354e3c1e3626946327b0fae316e63545baf
nemo <luoxincheng@baidu.com>
2016-05-26 16:08:51 +0800

    base function

---
+++ index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var url = require('url');
+
+var app = express();
+
+//环境变量
+app.set('port', 2233);
+// app.use(express.static(path.join(__dirname, 'views')))
+var arr = [];
+app.get('/', function(req, res) {
+	var html = '';
+	arr.forEach(function(one) {
+		html += '<a href="http://' + one.ip + ':' + one.port + '">' + one.name + '</a><br>';
+	})
+	res.send(html);
+});
+
+app.get('/getBird', function(req, res) {
+	var ipStr = req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
+    var ip = ipStr.match(/\d+\.\d+\.\d+\.\d+/)[0];
+    res.send('get')
+	arr.push({
+		ip: ip,
+		port: req.query.port,
+		name: req.query.name
+	})
+});
+
+// 启动及端口
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});
