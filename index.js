2d7c9bc5928deed5cb1f6630b5d6ace08a14a44f
nemo <luoxincheng@baidu.com>
2016-06-07 17:52:28 +0800

    no message

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var swig = require('swig');
 var checkIp = require('./lib/checkip');
 
 var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
 
 //环境变量
 app.set('port', 8301);
@@ -17,84 +19,108 @@ app.use(bodyParser.json());
 // 数组存放当前有哪些localhost被启动
 var feList = [];
 // feList-example:[{
-// 		ip: xxx,
-// 		port: xxx,
-// 		name: xxx,
-// 		demos: [{
-// 			path: desc
-// 		},{}]
+//      ip: xxx,
+//      port: xxx,
+//      name: xxx,
+//      demos: [{
+//          path: desc
+//      },{}]
 // }]
 // 
 app.get('/nemo', function(req, res) {
-	var html = swig.compileFile('views/index.html')({feList: feList})
-	res.send(html);
+    var html = swig.compileFile('views/index.html')({ feList: feList })
+    res.send(html);
 });
 app.get('/demo', function(req, res) {
-	var html = swig.compileFile('views/pages/demo.html')()
-	res.send(html);
+    var html = swig.compileFile('views/pages/demo.html')()
+    res.send(html);
 });
 app.get('*', function(req, res) {
-	res.redirect('/nemo')
+    res.redirect('/nemo')
 })
 
+var sendSocketMsg = function() {}
+io.sockets.on('connection', function(socket) {
+    sendSocketMsg = function() {
+        var html = swig.compileFile('views/pages/nav.html')({ feList: feList })
+        socket.emit('updateList', html);
+    }
+});
+
 // 用于获取并解析已开启的localhost并放入feList
+var ipTimeoutObj = {};
 app.post('/getBird', function(req, res) {
-	var repeat = false;
-	var ipStr = req.headers['x-forwarded-for'] ||
+    var repeat = false;
+    var ipStr = req.headers['x-forwarded-for'] ||
         req.connection.remoteAddress ||
         req.socket.remoteAddress ||
         req.connection.socket.remoteAddress;
     var ip = ipStr.match(/\d+\.\d+\.\d+\.\d+/)[0];
 
     if (req.body.port && req.body.name) {
-    	res.send('your info is ok');
-    }
-    else {
-    	res.send('please send right data');
-    	return false;
+        res.send('your info is ok');
+    } else {
+        res.send('please send right data');
+        return false;
     }
 
     // ip去重
     feList.forEach(function(one) {
-    	if (one.ip === ip) {
-    		repeat = true;
-    	}
+        if (one.ip === ip) {
+            repeat = true;
+        }
     })
-    if (repeat) {return;}
 
     // 把demos的key和value转成path和desc
     var demos = [];
     var j = 0;
-    for(var i in req.body.demos) {
-    	demos[j] = {};
-    	demos[j].path = i;
-    	demos[j].desc = req.body.demos[i];
-    	j++;
+    for (var i in req.body.demos) {
+        demos[j] = {};
+        demos[j].path = regPath(i);
+        demos[j].desc = req.body.demos[i];
+        j++;
     }
 
     // 添加localhost信息
-    feList.push({
-		ip: ip || req.body.ip,
-		ipType: checkIp(ip),
-		port: req.body.port,
-		name: req.body.name,
-		demos: demos
-	})
-	// 过20秒后删除。如果没有每隔20秒收到东西说明那个localhost挂了
-	setTimeout(function() {
-		removeBy(feList, 'ip', ip);
-	},20000)
+    if (!repeat) {
+        feList.push({
+            ip: ip || req.body.ip,
+            ipType: checkIp(ip),
+            port: req.body.port,
+            name: req.body.name,
+            demos: demos
+        })
+        
+        // socket更新页面列表
+        sendSocketMsg();
+    }
+
+    // 过30秒后删除。如果没有每隔20秒收到东西说明那个localhost挂了
+    clearTimeout(ipTimeoutObj[ip])
+    ipTimeoutObj[ip] = setTimeout(function() {
+        removeBy(feList, 'ip', ip);
+        sendSocketMsg();
+    }, 30000)
 });
 
 // 启动及端口
-http.createServer(app).listen(app.get('port'), function() {
+server.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'));
 });
 
 function removeBy(arr, key, val) {
-	arr.forEach(function(one) {
-		if (one[key] === val) {
-			arr.splice(arr.indexOf(one), 1);
-		}
-	})
+    arr.forEach(function(one) {
+        if (one[key] === val) {
+            arr.splice(arr.indexOf(one), 1);
+        }
+    })
+}
+
+function regPath(path) {
+    var first = path.toString()[0];
+    if (first === '/') {
+        return path.substr(1);
+    } else {
+        return path;
+    }
 }
--- static/js/index.js
+++ static/js/index.js
@@ -5,7 +5,13 @@ $('#nav').on('click', '.a', function(e) {
 })
 window.onload = function(e) {
 	$('#nav img').each(function(index, one) {
-		one.src = $(one).data('src');
+        var $this = $(one);
+		var img = new Image();
+        img.src = $(one).data('src');
+        img.onload = function(){
+            img.onload = null;
+            $this.attr('src', $(one).data('src'));
+        }
 	})
 }
 var toggleAnimate = {
@@ -69,6 +75,7 @@ $('#pencil').on('mouseup', function(e) {
 			$(this).addClass('lock');
 			toggleAnimate.paintBoxHide();
 			toggleAnimate.navShow();
+    		$('#cursor').css({'top': -999});
 		}
 	}
 	// console.log(e)
@@ -85,63 +92,146 @@ $('#pencil').on('mousemove', function(e) {
 
 // paint-box
 var paintColor = 'red';
+var paintRect = false;
+var eraser = false;
+var paintFunc = {
+	removeErase: function() {
+		$('#eraser').removeClass('eraser');
+		$('#cursor').css({'background': 'rgba(0,0,0,0)'})
+		eraser = false;
+	},
+	addErase: function() {
+		$('#eraser').addClass('eraser');
+		$('#cursor').css({'background': '#fff'})
+		eraser = true;
+	},
+	removeRect: function() {
+		$('#rect').removeClass('rect');
+		paintRect = false;
+	},
+	addRect: function() {
+		$('#rect').addClass('rect');
+		paintRect = true;
+	}
+}
 $('.color-select').on('click', function(e) {
     paintColor = $(e.target).attr('class');
+    paintFunc.removeErase();
     $('.color-select div').each(function () {
         $(this).removeClass('selected');
     })
-    $(e.target).addClass('selected')
+    $(e.target).addClass('selected');
+    $('#rect').css('border-color', paintColor);
 })
 $('#clear').on('click', function(e) {
 	clearArea();
+})
+$('#rect').on('click', function(e) {
+	paintFunc.removeErase();
+    if ($(this).hasClass('rect')) {
+		paintFunc.removeRect();
+	} else {
+		paintFunc.addRect();
+	}
+})
+$('#eraser').on('click', function() {
+	paintFunc.removeRect();
+	if ($(this).hasClass('eraser')) {
+		paintFunc.removeErase();
+	} else {
+		paintFunc.addErase();
+	}
 })
 
 // canvas
 var mousePressed = false;
 var lastX, lastY;
-var lineWidth = 5;
+var lineWidth = 10;
 var ctx;
 InitThis();
 function InitThis() {
     ctx = document.getElementById('canvas').getContext("2d");
- 	ctx.font="11px Georgia";
+ 	ctx.font = "12px Georgia";
  	$('#canvas').mousedown(function (e) {
         mousePressed = true;
         Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
+        startX = e.pageX - $(this).offset().left;
+        startY = e.pageY - $(this).offset().top;
     });
  
     $('#canvas').mousemove(function (e) {
     	if (mousePressed) {
             Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
+            if (paintRect) {
+            	if (e.pageX > startX) {
+            		$('#rectangle').css({
+            			'left': startX - lineWidth/2 + 'px', 
+            			'top': startY - lineWidth/2 + 'px', 
+            			'width': e.pageX - startX - lineWidth + 'px', 
+            			'height': e.pageY - startY - lineWidth + 'px',
+            			'border': 'solid ' +  lineWidth + 'px ' + paintColor
+            		})
+            	}
+            	else {
+            		$('#rectangle').css({
+            			'left': e.pageX - lineWidth/2 + 'px', 
+            			'top': e.pageY - lineWidth/2 + 'px', 
+            			'width': startX - e.pageX - lineWidth + 'px', 
+            			'height': startY - e.pageY  - lineWidth + 'px',
+            			'border': 'solid ' +  lineWidth + 'px ' + paintColor
+            		})
+            	}
+            }
+            if (eraser) {
+            	clearCircle(ctx, e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, lineWidth/2);
+            }
         }
-        $('#cursor').css({'top': e.pageY - lineWidth/2 + 'px', 'left': e.pageX - lineWidth/2 + 'px', 'width': lineWidth - 2 + 'px', 'height': lineWidth - 2 + 'px', 'border-color': paintColor})
+        $('#cursor').css({
+        	'top': e.pageY - lineWidth/2 + 'px', 
+        	'left': e.pageX - lineWidth/2 + 'px', 
+        	'width': lineWidth - 2 + 'px', 'height': lineWidth - 2 + 'px', 
+        	'border-color': paintColor
+        })
     });
  
     $('#canvas').mouseup(function (e) {
-        mousePressed = false;
+    	mousePressed = false;
+        endX = e.pageX - $(this).offset().left;
+        endY = e.pageY - $(this).offset().top;
+        drawRect(startX, startY, endX, endY);
+        $('#rectangle').css({'left': -999});
     });
     $('#canvas').mouseleave(function (e) {
         mousePressed = false;
     });
-    $('#canvas').on('mousewheel', function(e){
-    	var bold = lineWidth + e.originalEvent.wheelDelta/120;
+    $('#canvas').on('mousewheel MozMousePixelScroll', function(e){
+    	var bold = lineWidth + (e.originalEvent.wheelDelta/120 || -e.detail/16);
     	if (bold > 0 && bold < 200) {
-    		lineWidth = lineWidth + e.originalEvent.wheelDelta/120;
+    		lineWidth = lineWidth + (e.originalEvent.wheelDelta/120 || -e.detail/16);
     	}
-        $('#cursor').css({'top': e.pageY - lineWidth/2 + 'px', 'left': e.pageX - lineWidth/2 + 'px', 'width': lineWidth - 2 + 'px', 'height': lineWidth - 2 + 'px', 'border-color': paintColor})
+        $('#cursor').css({
+        	'top': (e.pageY || e.originalEvent.pageY) - lineWidth/2 + 'px', 
+        	'left': (e.pageX || e.originalEvent.pageX) - lineWidth/2 + 'px', 
+        	'width': lineWidth - 2 + 'px', 'height': lineWidth - 2 + 'px', 
+        	'border-color': paintColor
+        })
     })
     // 评论功能
     $('#canvas').dblclick(function(e) {
     	e.preventDefault();
     	$('#comment').css({'left': e.pageX, 'top': e.pageY - 12});
     	$('#comment input').focus();
-    	$('#cursor').css({'top': -999})
+    	$('#cursor').css({'top': -999});
     })
     $('#comment input').on('keydown blur', function(e){
     	if (e.keyCode === 13 || e.type === 'blur') {
     		var commentOffset = $('#comment').offset();
     		var canvasOffset = $('#canvas').offset();
-    		ctx.fillText($(this).val(), commentOffset.left - canvasOffset.left, commentOffset.top - canvasOffset.top + 12);
+    		ctx.fillText($(this).val(), commentOffset.left - canvasOffset.left, commentOffset.top - canvasOffset.top + 14);
+    		$(this).val('');
+    		$('#comment').css({'left': '-999px', 'top': '-999px'});
+    	}
+    	if (e.keyCode === 27) {
     		$(this).val('');
     		$('#comment').css({'left': '-999px', 'top': '-999px'});
     	}
@@ -149,6 +239,8 @@ function InitThis() {
 }
  
 function Draw(x, y, isDown) {
+	if (paintRect) { return; }
+	if (eraser) { return; }
 	if (isDown) {
         ctx.beginPath();
         ctx.strokeStyle = paintColor;
@@ -161,9 +253,21 @@ function Draw(x, y, isDown) {
     }
     lastX = x; lastY = y;
 }
-     
+function drawRect(startX, startY, endX, endY) {
+	if (!paintRect) { return; }
+	ctx.strokeStyle = paintColor;
+	ctx.lineWidth = lineWidth;
+	ctx.strokeRect(startX, startY, endX - startX, endY - startY)
+}
 function clearArea() {
     // Use the identity matrix while clearing the canvas
     ctx.setTransform(1, 0, 0, 1, 0, 0);
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 }  
+function clearCircle(oc,x,y,r){
+	for(var i=0; i< Math.round(Math.PI * r); i++){
+		var angle = (i / Math.round(Math.PI * r)) * 360;
+        oc.clearRect(x, y, Math.sin(angle * (Math.PI / 180)) * r , Math.cos(angle * (Math.PI / 180)) * r);
+    }
+}
+
